a3512b6995bfb1e35b1d5a37ff4a3cf877bfd244
enesyuksel7 <enes.yksel7@gmail.com>
2024-04-02 23:46:48 +0300

    Exam practice

---
+++ 9. Listbox+CRUD+Index/9. Listbox+CRUD+Index/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace _9.Listbox_CRUD_Index
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(403, 88);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(190, 554);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 22;
+            this.listBox2.Location = new System.Drawing.Point(615, 88);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(190, 554);
+            this.listBox2.TabIndex = 1;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad:";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(81, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 52);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(223, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 52);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(81, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 52);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(223, 399);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(119, 52);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Temizle";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numara:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(180, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 29);
+            this.textBox2.TabIndex = 8;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 22;
+            this.listBox3.Location = new System.Drawing.Point(828, 88);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(351, 554);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 22);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "AD";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(665, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 22);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "NUMARA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(969, 39);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(113, 22);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "LOG KAYDI";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(58, 496);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(300, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1206, 671);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ 9. Listbox+CRUD+Index/9. Listbox+CRUD+Index/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9.Listbox_CRUD_Index
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbTemizle()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            listBox2.Items.Add(textBox2.Text);
+            listBox3.Items.Add("Eklendi: " + DateTime.Now);
+            tbTemizle();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.SelectedIndex = -1;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int seciliIndex1 = listBox1.SelectedIndex;
+            int seciliIndex2 = listBox2.SelectedIndex;
+            if (seciliIndex1 > -1)
+            {
+                listBox1.Items[seciliIndex1] = textBox1.Text;
+                listBox2.Items[seciliIndex1] = textBox2.Text;
+            }
+            if(seciliIndex2 > -1)
+            {
+                listBox1.Items[seciliIndex2] = textBox1.Text;
+                listBox2.Items[seciliIndex2] = textBox2.Text;
+            }
+            listBox3.Items.Add("Güncellendi: " + DateTime.Now);
+            tbTemizle();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            int seciliIndex = listBox1.SelectedIndex;
+            textBox1.Text = listBox1.Items[seciliIndex].ToString();
+            textBox2.Text = listBox2.Items[seciliIndex].ToString();
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+            int seciliIndex = listBox2.SelectedIndex;
+            textBox1.Text = listBox1.Items[seciliIndex].ToString();
+            textBox2.Text = listBox2.Items[seciliIndex].ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int secilenIndex1 = listBox1.SelectedIndex;
+            int secilenIndex2 = listBox2.SelectedIndex;
+            if (secilenIndex1 > -1)
+            {
+                listBox1.Items.RemoveAt(secilenIndex1);
+                listBox2.Items.RemoveAt(secilenIndex1);
+            }
+            if(secilenIndex2 > -1)
+            {
+                listBox1.Items.RemoveAt(secilenIndex2);
+                listBox2.Items.RemoveAt(secilenIndex2);
+            }
+            listBox3.Items.Add("Silindi: " + DateTime.Now);
+            tbTemizle();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Add("Temizlendi: " + DateTime.Now);
+            tbTemizle();
+        }
+    }
+}
